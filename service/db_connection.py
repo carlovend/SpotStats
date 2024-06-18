0369739782b67d702adae1c1b2aa3996b2714312
carlovend <carlovenditto28@gmail.com>
2024-06-18 16:39:34 +0200

    Add query_manager

--- service/db_connection.py
+++ service/db_connection.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pymongo import MongoClient
 import os
 
+
 # find correct dataset path
 package_path = os.path.dirname(__file__)
 root_path = os.path.dirname(package_path)
@@ -17,5 +18,43 @@ db = client['SpotyStats']
 songs_collection = db['Songs']
 artists_collection = db['Artists']
 
-# Inserisci il dizionario nel database MongoDB
-collection.insert_many(data)
+# Funzione per convertire i dati in documenti per MongoDB
+def create_documents(df):
+    songs = []
+    artists = {}
+
+    for _, row in df.iterrows():
+        # Creazione del documento per la canzone
+        song = {
+            "song_id": row["Song ID"],
+            "name": row["Song Name"],
+            "artist_id": row["Artist"],
+            "highest_position": row["Highest Charting Position"],
+            "weeks_on_chart": row["Number of Times Charted"],
+            "streams": int(row["Streams"].replace(',', '')),
+            "release_date": row["Release Date"],
+            "popularity": row["Popularity"],
+            "key": row["Chord"],
+        }
+        songs.append(song)
+
+        # Creazione del documento per l'artista se non già presente
+        if row["Artist"] not in artists:
+            artists[row["Artist"]] = {
+                "artist_id": row["Artist"],
+                "name": row["Artist"],
+                "followers": row["Artist Followers"],
+                "genres": row["Genre"]  # Convertire la stringa di generi in una lista
+            }
+
+    return songs, list(artists.values())
+
+
+# Creare i documenti per le collezioni
+songs, artists = create_documents(df)
+
+# Inserire i documenti nelle collezioni
+songs_collection.insert_many(songs)
+artists_collection.insert_many(artists)
+
+print("Documenti inseriti con successo!")
